9931d380366af72280edd3008218a5994324ee8c
Vishesh Handa <me@vhanda.in>
2020-02-12 00:22:03 +0100

    Add basic array support

--- lib/yaml_serializer.dart
+++ lib/yaml_serializer.dart
@@ -16,21 +16,34 @@ String toYAML(Map<String, dynamic> map) {
   var str = '';
 
   map.forEach((key, value) {
-    var val = value.toString();
-    var mustEscape = false;
-    for (var s in plainStringNotAllowed) {
-      if (val.contains(s)) {
-        mustEscape = true;
-        break;
+    var val = '';
+    if (value is List) {
+      var items = <String>[];
+      for (var v in value) {
+        items.add(_toYamlString(v));
       }
-    }
-    if (mustEscape) {
-      if (val.contains("'")) {
-        val = val.replaceAll("'", "''");
-      }
-      val = "'" + val + "'";
+      val = '[' + items.join(', ') + ']';
+    } else {
+      val = _toYamlString(value.toString());
     }
     str += key + ': ' + val + '\n';
   });
   return str;
+}
+
+String _toYamlString(String val) {
+  var mustEscape = false;
+  for (var s in plainStringNotAllowed) {
+    if (val.contains(s)) {
+      mustEscape = true;
+      break;
+    }
+  }
+  if (mustEscape) {
+    if (val.contains("'")) {
+      val = val.replaceAll("'", "''");
+    }
+    val = "'" + val + "'";
+  }
+  return val;
 }
